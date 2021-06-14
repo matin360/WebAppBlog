95e22906e8cbd5d03162d96b09dca3578a05e85a
Матин Манафов <matin.manafovvvv@gmail.com>
2021-06-14 20:21:27 +0400

    faahion page added

---
+++ WebAppBlog/BlogWeb/BlogWeb.WebUI/Controllers/FashionController.cs
@@ -0,0 +1,26 @@
+﻿using BlogWeb.Domain.Concrete;
+using BlogWeb.WebUI.Infrastructure;
+using BlogWeb.WebUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BlogWeb.WebUI.Controllers
+{
+    public class FashionController : Controller
+    {
+        private BlogWebDbContext _dbContext;
+        private int _itemsPerPage;
+
+        public FashionController()
+        {
+            _dbContext = new BlogWebDbContext();
+            _itemsPerPage = 6;
+        }
+        [HttpGet]
+        public async Task<ActionResult> Index(PageModel model) => View(await _dbContext.GetPaginatablePostsAsync(_itemsPerPage, model));
+    }
+}
